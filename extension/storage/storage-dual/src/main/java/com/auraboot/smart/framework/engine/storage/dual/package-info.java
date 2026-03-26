0a25b954bdd0e47e6d37f0429b65be7731b69809
diqi <ghjapple@163.com>
2026-03-26 22:51:09 +0800

    build: add package-info.java to storage-dual for Central Portal source/javadoc requirement

---
+++ extension/storage/storage-dual/src/main/java/com/auraboot/smart/framework/engine/storage/dual/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Dual-mode storage: combines custom (in-memory) and database storage,
+ * allowing runtime switching between modes.
+ */
+package com.auraboot.smart.framework.engine.storage.dual;
